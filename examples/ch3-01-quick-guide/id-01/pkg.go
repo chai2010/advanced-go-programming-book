537decf3bde118930f7803c495275464f43d2f9c
chai2010 <chaishushan@gmail.com>
2018-05-21 06:51:38 +0800

    ch3-01: 完善

---
+++ examples/ch3-01-quick-guide/id-01/pkg.go
@@ -0,0 +1,3 @@
+package pkg
+
+var Id = 9527
---
+++ examples/ch3-01-quick-guide/id-02/pkg_amd64.s
@@ -0,0 +1,11 @@
+
+DATA ·Id+0(SB)/1,$0x37
+DATA ·Id+1(SB)/1,$0x25
+DATA ·Id+2(SB)/1,$0x00
+DATA ·Id+3(SB)/1,$0x00
+DATA ·Id+4(SB)/1,$0x00
+DATA ·Id+5(SB)/1,$0x00
+DATA ·Id+6(SB)/1,$0x00
+DATA ·Id+7(SB)/1,$0x00
+
+GLOBL ·Id(SB),$8
