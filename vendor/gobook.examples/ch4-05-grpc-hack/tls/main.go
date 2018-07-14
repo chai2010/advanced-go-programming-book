e297c95ca370bfc8db61ebcd9c9279ec9a12e1e5
chai2010 <chaishushan@gmail.com>
2018-07-14 13:08:57 +0800

    vendor: 调整grpc例子

--- vendor/gobook.examples/ch4-05-grpc-hack/tls/main.go
+++ vendor/gobook.examples/ch4-05-grpc-hack/tls/main.go
@@ -12,22 +12,24 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"google.golang.org/grpc/reflection"
 )
 
 var (
 	port = ":5000"
 
-	ca         = "./ca.cer"
-	server_crt = "./server.cer"
-	server_key = "./server_pri.key"
-	client_crt = "./client.cer"
-	client_key = "./client_pri.key"
+	tlsDir        = "./tls-config"
+	tlsServerName = "server"
+
+	ca         = tlsDir + "/ca.cer"
+	server_crt = tlsDir + "/server.cer"
+	server_key = tlsDir + "/server_pri.key"
+	client_crt = tlsDir + "/client.cer"
+	client_key = tlsDir + "/client_pri.key"
 )
 
-type server struct{}
+type myGrpcServer struct{}
 
-func (s *server) SayHello(ctx context.Context, in *HelloRequest) (*HelloReply, error) {
+func (s *myGrpcServer) SayHello(ctx context.Context, in *HelloRequest) (*HelloReply, error) {
 	return &HelloReply{Message: "Hello " + in.Name}, nil
 }
 
@@ -39,13 +41,11 @@ func main() {
 }
 
 func startServer() {
-	// Load the certificates from disk
 	certificate, err := tls.LoadX509KeyPair(server_crt, server_key)
 	if err != nil {
 		log.Panicf("could not load server key pair: %s", err)
 	}
 
-	// Create a certificate pool from the certificate authority
 	certPool := x509.NewCertPool()
 	ca, err := ioutil.ReadFile(ca)
 	if err != nil {
@@ -57,12 +57,6 @@ func startServer() {
 		log.Panic("failed to append client certs")
 	}
 
-	// Create the channel to listen on
-	lis, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Panicf("could not list on %s: %s", port, err)
-	}
-
 	// Create the TLS credentials
 	creds := credentials.NewTLS(&tls.Config{
 		ClientAuth:   tls.RequireAndVerifyClientCert,
@@ -70,21 +64,16 @@ func startServer() {
 		ClientCAs:    certPool,
 	})
 
-	// Create the gRPC server with the credentials
-	s := grpc.NewServer(grpc.Creds(creds))
+	server := grpc.NewServer(grpc.Creds(creds))
+	RegisterGreeterServer(server, new(myGrpcServer))
 
-	// Register the handler object
-	RegisterGreeterServer(s, &server{})
-
-	// Serve and Listen
-	if err := s.Serve(lis); err != nil {
-		log.Panicf("grpc serve error: %s", err)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Panicf("could not list on %s: %s", port, err)
 	}
 
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	if err := server.Serve(lis); err != nil {
+		log.Panicf("grpc serve error: %s", err)
 	}
 }
 
@@ -94,25 +83,21 @@ func doClientWork() {
 		log.Panicf("could not load client key pair: %s", err)
 	}
 
-	// Create a certificate pool from the certificate authority
 	certPool := x509.NewCertPool()
 	ca, err := ioutil.ReadFile(ca)
 	if err != nil {
 		log.Panicf("could not read ca certificate: %s", err)
 	}
-
-	// Append the certificates from the CA
 	if ok := certPool.AppendCertsFromPEM(ca); !ok {
 		log.Panic("failed to append ca certs")
 	}
 
 	creds := credentials.NewTLS(&tls.Config{
-		ServerName:   "server", // NOTE: this is required!
+		ServerName:   tlsServerName, // NOTE: this is required!
 		Certificates: []tls.Certificate{certificate},
 		RootCAs:      certPool,
 	})
 
-	// Create a connection with the TLS credentials
 	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatal(err)
@@ -120,6 +105,7 @@ func doClientWork() {
 	defer conn.Close()
 
 	c := NewGreeterClient(conn)
+
 	r, err := c.SayHello(context.Background(), &HelloRequest{Name: "gopher"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
