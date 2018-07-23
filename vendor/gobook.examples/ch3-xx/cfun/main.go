a4a33d597f260f110fd29b9e56e7ffb49e71ec85
chai2010 <chaishushan@gmail.com>
2018-07-23 11:37:48 +0800

    vendor: fix cfun

--- vendor/gobook.examples/ch3-xx/cfun/main.go
+++ vendor/gobook.examples/ch3-xx/cfun/main.go
@@ -1,36 +1,31 @@
 package main
 
 /*
-#include <stdio.h>
 #include <stdint.h>
 
-int64_t add(int64_t a, int64_t b) {
+int64_t myadd(int64_t a, int64_t b) {
 	return a+b;
-}
-void print_add_addr() {
-	printf("%x\n", (int)(add));
 }
 */
 import "C"
+
 import (
 	"asmpkg"
 	"fmt"
+	"runtime"
 	"unsafe"
 )
 
-//go:noinline
-//go:nosplit
 func main() {
-	println(C.add(1, 2))
-
-	C.print_add_addr()
-	fmt.Printf("%x\n", uintptr(unsafe.Pointer(C.add)))
-
-	if true {
-		c := asmpkg.CallCAdd(
-			uintptr(unsafe.Pointer(C.add)),
-			1, 2,
-		)
-		fmt.Printf("result: %x\n", c)
+	if runtime.GOOS == "windows" {
+		fmt.Println(asmpkg.CallCAdd_Win64_ABI(
+			uintptr(unsafe.Pointer(C.myadd)),
+			123, 456,
+		))
+	} else {
+		fmt.Println(asmpkg.CallCAdd_SystemV_ABI(
+			uintptr(unsafe.Pointer(C.myadd)),
+			123, 456,
+		))
 	}
 }
--- vendor/gobook.examples/ch3-xx/cfun/vendor/asmpkg/asmpkg.go
+++ vendor/gobook.examples/ch3-xx/cfun/vendor/asmpkg/asmpkg.go
@@ -1,3 +1,4 @@
 package asmpkg
 
-func CallCAdd(cfun uintptr, a, b int64) int64
+func CallCAdd_SystemV_ABI(cfun uintptr, a, b int64) int64
+func CallCAdd_Win64_ABI(cfun uintptr, a, b int64) int64
